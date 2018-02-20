c68ca59885ece5a129be72f1e27a6e9d56301e33
fanny <fc2@cin.ufpe.br>
2018-02-19 23:20:24 -0300

    Dropping elements

--- script.js
+++ script.js
@@ -38,3 +38,20 @@ function findElement(arr, func) {
 findElement([1, 2, 3, 4], function (num) { return num % 2 === 0; });
 
 
+// --- Drop it! ---
+//Drop the elements of an array, starting from the front, until the predicate (second argument) returns true.
+
+function dropElements(arr, func) {
+    // Drop them elements.
+
+
+    for (var i = 0; i < arr.length; i++) {
+        if (func(arr[i])) {
+            return arr.slice(i);
+        }
+    }
+    return [];
+
+}
+
+dropElements([1, 2, 3], function (n) { return n < 3; });

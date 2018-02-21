2c49380c411bed9921975a2d691f31cd02a18202
fanny <fc2@cin.ufpe.br>
2018-02-21 16:59:47 -0300

    Record Collection

--- script.js
+++ script.js
@@ -135,6 +135,59 @@ function addTogether() {
     } else {
         return undefined;
     }
-} 
-addTogether(2)([3]) ;
+}
+addTogether(2)([3]);
 //need a better solution!!
+
+
+// --- Record Collection --- 
+
+// Setup
+var collection = {
+    "2548": {
+      "album": "Slippery When Wet",
+      "artist": "Bon Jovi",
+      "tracks": [ 
+        "Let It Rock", 
+        "You Give Love a Bad Name" 
+      ]
+    },
+    "2468": {
+      "album": "1999",
+      "artist": "Prince",
+      "tracks": [ 
+        "1999", 
+        "Little Red Corvette" 
+      ]
+    },
+    "1245": {
+      "artist": "Robert Palmer",
+      "tracks": [ ]
+    },
+    "5439": {
+      "album": "ABBA Gold"
+    }
+};
+// Keep a copy of the collection for tests
+var collectionCopy = JSON.parse(JSON.stringify(collection));
+
+// Only change code below this line
+function updateRecords(id, prop, value) {
+  
+    if (prop === "tracks" && value !== ""){
+        if(collectionCopy[id].tracks == null){
+            collectionCopy[id].tracks = [value];
+        } else{
+            collectionCopy[id].tracks.push(value);
+        }
+    } else if (value !== ""){
+        collectionCopy[id][prop] = value;
+    } else {
+        delete collectionCopy[id][prop];
+    }
+
+  return collectionCopy;
+}
+
+// Alter values below to test your code
+updateRecords(5439, "artist", "ABBA");

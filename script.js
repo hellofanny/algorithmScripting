6ac59f0e740899fc8855d13503c5de95b6a93ef6
fanny <fc2@cin.ufpe.br>
2018-02-20 22:55:30 -0300

    Binary Agents

--- script.js
+++ script.js
@@ -81,3 +81,20 @@ function steamrollArray(arr) {
 }
 
 steamrollArray([1, [2], [3, [[4]]]]);
+
+
+// -- Binary Agents -- 
+
+function binaryAgent(str) {
+
+    var binaryArray = str.split(" ");
+
+    var decodeArray = binaryArray.map(function (elem) {
+        return String.fromCharCode(parseInt(elem, 2));
+    });
+
+    return decodeArray.join("");
+}
+
+binaryAgent("01000001 01110010 01100101 01101110 00100111 01110100 00100000 01100010 01101111 01101110 01100110 01101001 01110010 01100101 01110011 00100000 01100110 01110101 01101110 00100001 00111111");
+
